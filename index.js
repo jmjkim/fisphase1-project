8cd09706f8dc72812bfbae95354b9b02d8d7855c
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-03-23 19:08:21 -0700

    Adds editing feature on employees, Refactored code efficiency

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function dataFetcher() {
 
 
 function dataDisplayer(data) {
-    // Refines data (Object) into simple format
     const refinedData = data.map((obj) => (({
         avatar, 
         id, 
@@ -34,10 +33,10 @@ function dataDisplayer(data) {
            last: last_name, 
            ssn: social_insurance_number, 
            dob: date_of_birth, 
-           address: `${address.street_address} ${address.city} ${address.state} ${address.zip_code.split("-")[0]}`, 
+           address: `${address.street_address} ${address.city} ${address.state} ${address.zip_code}`, 
            email, 
            phone: phone_number, 
-           position: employment.title
+           position: `${employment.title}` 
        }))(obj))
 
 
@@ -49,16 +48,16 @@ function dataDisplayer(data) {
     const categoryDiv = document.createElement("div") 
     const detailDiv = document.createElement("div") 
     const img = document.createElement("img")
-
     const editBtn = document.createElement("button")
+    const submitEdit = document.createElement("button")
     
     mainContainer.appendChild(employeeDiv)
     employeeDiv.appendChild(img)
     employeeDiv.appendChild(subContainer)
     subContainer.appendChild(categoryDiv)
     subContainer.appendChild(detailDiv)
-
     employeeDiv.appendChild(editBtn)
+    employeeDiv.appendChild(submitEdit)
     
     img.setAttribute("src", obj.photo)
     employeeDiv.setAttribute("class", "employee_div")
@@ -66,10 +65,13 @@ function dataDisplayer(data) {
     subContainer.setAttribute("class", "subContainer")
     categoryDiv.setAttribute("class", "category_div")
     detailDiv.setAttribute("class", "detail_div")
-
-    editBtn.setAttribute("class", "edit_btn")
+    editBtn.setAttribute("class", "button")
     editBtn.setAttribute("id", `edit_btn_${obj.id}`)
+    submitEdit.setAttribute("class", "button")
+    submitEdit.setAttribute("id", `submit_edit_${obj.id}`)
+
     editBtn.innerText = "Edit"
+    submitEdit.innerText = "Update"
 
 
     // Creates Categories & Detail contents
@@ -83,8 +85,13 @@ function dataDisplayer(data) {
             
             categoryLi.insertAdjacentText("afterbegin", entry[0].toUpperCase())
             detailLi.insertAdjacentText("afterbegin", entry[1])
+
+            if (entry[0] === "ssn" || entry[0] === "address" || entry[0] === "dob") {
+                detailLi.setAttribute("class", "pid")
+            }
         }})
-    dataEditor(editBtn, obj.id)    
+
+    dataEditor(editBtn, submitEdit, obj.id)    
     })
 }
 
@@ -104,17 +111,63 @@ function dataFinder() {
 }
 
 
-function dataEditor(btn, targetId) {
-    btn.addEventListener("click", (e) => {
+function dataEditor(editBtn, submitEdit, targetId) {    
+    editBtn.addEventListener("click", (e) => {
         const target = document.querySelector(`#employee_${targetId}`)
         const detailLi = target.childNodes[1].childNodes[1]
         detailLi.setAttribute("contenteditable", true)
+        detailLi.setAttribute("class", "edit_mode")
 
         detailLi.addEventListener("keydown", (e) => {
             if (e.code === "Escape") {
                 detailLi.setAttribute("contenteditable", false)
+                detailLi.setAttribute("class", "detail_div")
             }
         })
+
+        e.preventDefault()
+    })
+
+    submitEdit.addEventListener("click", (e) => {
+        const target = document.querySelector(`#employee_${targetId}`)
+        const detailLi = target.childNodes[1].childNodes[1]
+
+        detailLi.setAttribute("contenteditable", false)
+        detailLi.setAttribute("class", "detail_div")
+
+        const splitAddress = detailLi.childNodes[5].innerText.split(" ")
+        const streetAddress = `${splitAddress[0]} ${splitAddress[1]} ${splitAddress[2]}`
+        const cityOf = `${splitAddress[3]} ${splitAddress[4]}`
+        const stateOf = splitAddress[5]
+        const zip = splitAddress[6]
+
+        
+        fetch(`http://localhost:3000/employees/${targetId}`, ({
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+                id: detailLi.childNodes[0].innerText, 
+                first_name: detailLi.childNodes[1].innerText, 
+                last_name: detailLi.childNodes[2].innerText, 
+                social_insurance_number: detailLi.childNodes[3].innerText, 
+                date_of_birth: detailLi.childNodes[4].innerText, 
+                address: {
+                    city: cityOf,
+                    street_address: streetAddress,
+                    zip_code: zip,
+                    state: stateOf
+                },
+                email: detailLi.childNodes[6].innerText,
+                phone_number: detailLi.childNodes[7].innerText,
+                employment: {
+                    title: detailLi.childNodes[8].innerText,
+                }})
+            })
+        )
+
         e.preventDefault()
     })
 }
