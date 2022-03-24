2f24f97107c4cce9559228049b4cade60848395b
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-03-24 13:57:57 -0700

    Refactors code efficiency

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 // https://random-data-api.com/api/users/random_user
 
 document.addEventListener("DOMContentLoaded", () => { // EventListener (1/3)
-    dataFetcher()
+    dataFetcher(dataDisplayer)
     dataFinder()
 })
 
-
-function dataFetcher() {
+function dataFetcher(callback) {
     fetch("http://localhost:3000/employees")
     .then(res => res.json())
-    .then(data => dataDisplayer(data))
+    .then(callback)
     .catch(error => console.error(error))
 }
-
 
 function dataDisplayer(data) {
     const refinedData = data.map((obj) => (({
@@ -38,7 +36,6 @@ function dataDisplayer(data) {
            phone: phone_number, 
            position: `${employment.title}` 
        }))(obj))
-
 
     // Creates employee containers
     refinedData.forEach(obj => {
@@ -73,7 +70,6 @@ function dataDisplayer(data) {
     editBtn.innerText = "Edit"
     submitEdit.innerText = "Update"
 
-
     // Creates Categories & Detail contents
     Object.entries(obj).forEach(entry => {
         const categoryLi = document.createElement("li")
@@ -90,31 +86,29 @@ function dataDisplayer(data) {
                 detailLi.setAttribute("class", "pid")
             }
         }})
-
     dataEditor(editBtn, submitEdit, obj.id)    
     })
 }
 
-
 function dataFinder() {
     const form = document.querySelector("#employee_finder")
-
+    
     form.addEventListener("submit", (e) => { // EventListener (2/3)
         const target = document.querySelector(`#employee_${+e.target[0].value}`)
 
         target.scrollIntoView(true)
         target.setAttribute("class", "search_result") // Simple interactivity: Highlights on searched employee
         target.addEventListener("click", () => target.setAttribute("class", "employee_div")) // Removes highlight
-
+        
         e.preventDefault()
     })
 }
 
-
 function dataEditor(editBtn, submitEdit, targetId) {    
-    editBtn.addEventListener("click", (e) => {
-        const target = document.querySelector(`#employee_${targetId}`)
-        const detailLi = target.childNodes[1].childNodes[1]
+    const target = document.querySelector(`#employee_${targetId}`)
+    const detailLi = target.childNodes[1].childNodes[1]
+
+    editBtn.addEventListener("click", () => {
         detailLi.setAttribute("contenteditable", true)
         detailLi.setAttribute("class", "edit_mode")
 
@@ -124,24 +118,15 @@ function dataEditor(editBtn, submitEdit, targetId) {
                 detailLi.setAttribute("class", "detail_div")
             }
         })
-
-        e.preventDefault()
     })
 
     submitEdit.addEventListener("click", (e) => {
-        const target = document.querySelector(`#employee_${targetId}`)
-        const detailLi = target.childNodes[1].childNodes[1]
-
         detailLi.setAttribute("contenteditable", false)
         detailLi.setAttribute("class", "detail_div")
 
         const splitAddress = detailLi.childNodes[5].innerText.split(" ")
-        const streetAddress = `${splitAddress[0]} ${splitAddress[1]} ${splitAddress[2]}`
-        const cityOf = `${splitAddress[3]} ${splitAddress[4]}`
-        const stateOf = splitAddress[5]
-        const zip = splitAddress[6]
+        const liChildNode = detailLi.childNodes
 
-        
         fetch(`http://localhost:3000/employees/${targetId}`, ({
             method: "PATCH",
             headers: {
@@ -149,25 +134,24 @@ function dataEditor(editBtn, submitEdit, targetId) {
                 Accept: "application/json"
             },
             body: JSON.stringify({
-                id: detailLi.childNodes[0].innerText, 
-                first_name: detailLi.childNodes[1].innerText, 
-                last_name: detailLi.childNodes[2].innerText, 
-                social_insurance_number: detailLi.childNodes[3].innerText, 
-                date_of_birth: detailLi.childNodes[4].innerText, 
+                id: liChildNode[0].innerText, 
+                first_name: liChildNode[1].innerText, 
+                last_name: liChildNode[2].innerText, 
+                social_insurance_number: liChildNode[3].innerText, 
+                date_of_birth: liChildNode[4].innerText, 
                 address: {
-                    city: cityOf,
-                    street_address: streetAddress,
-                    zip_code: zip,
-                    state: stateOf
+                    city: `${splitAddress[3]} ${splitAddress[4]}`,
+                    street_address: `${splitAddress[0]} ${splitAddress[1]} ${splitAddress[2]}`,
+                    zip_code: splitAddress[6],
+                    state: splitAddress[5]
                 },
-                email: detailLi.childNodes[6].innerText,
-                phone_number: detailLi.childNodes[7].innerText,
+                email: liChildNode[6].innerText,
+                phone_number: liChildNode[7].innerText,
                 employment: {
-                    title: detailLi.childNodes[8].innerText,
+                    title: liChildNode[8].innerText,
                 }})
             })
         )
-
         e.preventDefault()
     })
 }
